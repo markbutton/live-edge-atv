ffe36178398094a025f2123140f5539700b71b68
Mark Button <mark.button@gmail.com>
2020-08-12 17:04:17 -0600

    rework subdomain

--- client/src/environments/environment.dev.ts
+++ client/src/environments/environment.dev.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: false,
   appId: 'f9ae7ad578a6d06724a140168b4dfc95',
-  baseUrl: 'https://api.develop.atv.spectrumtoolbox.com/api/',
-  jobAPIUrl: 'https://schedule-job.develop.atv.spectrumtoolbox.com/api/',
-  docsUrl: 'https://api.develop.atv.spectrumtoolbox.com/api-docs/',
-  busUrl: 'https://develop-bus.atv.spectrumtoolbox.com/',
-  socketUrl: 'https://api.develop.atv.spectrumtoolbox.com/',
-  sourceVideoUrl: 'https://api.develop.atv.spectrumtoolbox.com/api/s3',
-  targetVideoUrl: 'https://api.develop.atv.spectrumtoolbox.com/api/s3',
-  baseHref: 'https://ui.develop.atv.spectrumtoolbox.com/',
-  logsUrl: 'https://log-api.develop.atv.spectrumtoolbox.com/logs/',
-  logSocket: 'https://log-api.develop.atv.spectrumtoolbox.com/',
+  baseUrl: 'https://atv-api.develop.reach.spectrumtoolbox.com/api/',
+  jobAPIUrl: 'https://schedule-job.develop.reach.spectrumtoolbox.com/api/',
+  docsUrl: 'https://atv-api.develop.reach.spectrumtoolbox.com/api-docs/',
+  busUrl: 'https://develop-bus.reach.spectrumtoolbox.com/',
+  socketUrl: 'https://atv-api.develop.reach.spectrumtoolbox.com/',
+  sourceVideoUrl: 'https://atv-api.develop.reach.spectrumtoolbox.com/api/s3',
+  targetVideoUrl: 'https://atv-api.develop.reach.spectrumtoolbox.com/api/s3',
+  baseHref: 'https://atv.develop.reach.spectrumtoolbox.com/',
+  logsUrl: 'https://log-api.develop.reach.spectrumtoolbox.com/logs/',
+  logSocket: 'https://log-api.develop.reach.spectrumtoolbox.com/',
   apiHeaderKey: 'x-charter-api-key',
   apiHeaderValue: 'live_edge@2019',
-  polyUrl: 'https://extract-zone-poly.develop.atv.spectrumtoolbox.com/api/'
+  polyUrl: 'https://extract-zone-poly.develop.reach.spectrumtoolbox.com/api/'
 };
--- client/src/environments/environment.qa.ts
+++ client/src/environments/environment.qa.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: false,
   appId: 'f9ae7ad578a6d06724a140168b4dfc95',
-  baseUrl: 'https://api.test.atv.spectrumtoolbox.com/api/',
-  jobAPIUrl: 'https://schedule-job.test.atv.spectrumtoolbox.com/api/',
-  docsUrl: 'https://api.test.atv.spectrumtoolbox.com/api-docs/',
-  busUrl: 'https://bus.test.atv.spectrumtoolbox.com/',
-  socketUrl: 'https://api.test.atv.spectrumtoolbox.com/',
-  sourceVideoUrl: 'https://api.test.atv.spectrumtoolbox.com/api/s3',
-  targetVideoUrl: 'https://api.test.atv.spectrumtoolbox.com/api/s3',
-  baseHref: 'https://ui.test.atv.spectrumtoolbox.com',
-  logsUrl: 'https://log-api.test.atv.spectrumtoolbox.com/logs/',
-  logSocket: 'https://log-api.test.atv.spectrumtoolbox.com/',
+  baseUrl: 'https://atv-api.test.reach.spectrumtoolbox.com/api/',
+  jobAPIUrl: 'https://schedule-job.test.reach.spectrumtoolbox.com/api/',
+  docsUrl: 'https://atv-api.test.reach.spectrumtoolbox.com/api-docs/',
+  busUrl: 'https://bus.test.reach.spectrumtoolbox.com/',
+  socketUrl: 'https://atv-api.test.reach.spectrumtoolbox.com/',
+  sourceVideoUrl: 'https://atv-api.test.reach.spectrumtoolbox.com/api/s3',
+  targetVideoUrl: 'https://atv-api.test.reach.spectrumtoolbox.com/api/s3',
+  baseHref: 'https://atv.test.reach.spectrumtoolbox.com',
+  logsUrl: 'https://log-api.test.reach.spectrumtoolbox.com/logs/',
+  logSocket: 'https://log-api.test.reach.spectrumtoolbox.com/',
   apiHeaderKey: 'x-charter-api-key',
   apiHeaderValue: 'live_edge@2019',
-  polyUrl: 'https://extract-zone-poly.test.atv.spectrumtoolbox.com/api/'
+  polyUrl: 'https://extract-zone-poly.test.reach.spectrumtoolbox.com/api/'
 };
